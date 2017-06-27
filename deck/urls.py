297201c2d3a58e86b2095a273867ebc58003ad47
JBT <moskvant.ai@gmail.com>
2017-06-27 18:14:57 +0300

    ML for matias

--- deck/urls.py
+++ deck/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^get_more_tables/',views.get_more_tables, name='get_more_tables'),
+    url(r'^ML/',views.ml,name='ML'),
 ]
--- deck/views.py
+++ deck/views.py
@@ -15,8 +15,21 @@ def index(request):
     obj = requests.get(url)
     #data = json.loads(obj.content)
     data = json.loads(obj.content.decode('utf-8'))
+    market_data = data[1]['securities'][1]
     # df1 = pd.DataFrame(data['securities']['data'],columns=data['securities']['metadata'])
-    return render(request,'deck/deck_home.html',{'data':data[1]['securities'][1]})
+    return render(request,'deck/deck_home.html',{'data':data[1]['securities'][1],'market_data':market_data})
+
+
+
+def get_more_tables(request):
+    increment = int(request.GET['append_increment'])
+    increment_to = increment + 10
+    order = [increment_to,5,6]
+    # order = Order.objects.filter(owner=request.user).order_by('-id')[increment:increment_to]
+    return render(request, 'deck/deck_table.html', {'order': order})
+
+def ml(request):
+    return render(request,'deck/ML.html')
 
 
 # url = 'http://iss.moex.com/iss/engines/futures/markets/forts/securities.json?'
